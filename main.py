29c792c8e488328b0ab6ed7db7b90b59673534cc
Yihao129 <yyhxzl@gmail.com>
2021-01-01 22:34:17 +0800

    show online users

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request, send_from_directory
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 from flask_cors import cross_origin, CORS
 from urllib.parse import unquote
 
+total_online = 0
 
 app = Flask(__name__, static_url_path='')
 CORS(app)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app, cors_allowed_origins='*')
+
+
 
 @socketio.on('message')
 def handleMessage(msg):
@@ -16,6 +19,23 @@ def handleMessage(msg):
     print('Message: ' + m)
     send(msg, broadcast=True)
 
+@socketio.on('connected')
+def handleConnected(msg):
+    print("A user comes.")
+    global total_online
+    total_online += 1
+    print("current total:", total_online)
+    emit("updateOnlineCount", total_online, broadcast=True)
+    
+    
+@socketio.on('disconnected')
+def handleDisconnected(msg):
+    print("A user leaves.")
+    global total_online
+    total_online -= 1
+    print("emitting total")
+    emit("updateOnlineCount", total_online, broadcast=True)
+    
 
 @cross_origin()
 @app.route('/', methods=['GET', 'POST'])
@@ -29,4 +49,4 @@ def send_js(path):
     return send_from_directory('lib', path)
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", port="80")
+    socketio.run(app, host="0.0.0.0", port=80)
